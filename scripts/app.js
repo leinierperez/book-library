bc18e292cc71d93e4262440b69abb8947b3f97e4
Leinier Perez <P_Leinier@yahoo.com>
2020-09-02 04:29:09 -0400

    Refactor html and css to fix certain issues and added close button on form. Add js code to get values from the form

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,12 @@
+Book1 = new Book("rambo", "rambot", 325, false);
+Book2 = new Book("rbncvn", "ramotnbmbn", 354, true);
+Book3 = new Book("ramgfdg", "rboarew", 341, true);
+
 let myLibrary = [];
+
+// myLibrary.push(Book1);
+// myLibrary.push(Book2);
+// myLibrary.push(Book3);
 
 function Book(title, author, pages, isRead) {
   this.title = title;
@@ -13,9 +21,59 @@ const removeBook = document.querySelector(".remove");
 const readToggle = document.querySelector(".toggle_read");
 const form = document.querySelector(".form_popup");
 const submit = document.getElementById("submit");
+const formTitle = document.getElementById("form_title");
+const formAuthor = document.getElementById("form_author");
+const formPages = document.getElementById("form_pages");
+const readCheckBox = document.getElementById("isRead");
+const cardTitle = document.getElementById("title");
+const cardAuthor = document.getElementById("author");
+const cardPages = document.getElementById("pages");
+const closeBtn = document.querySelector(".close_btn");
 
 btn.addEventListener("click", (e) => {
   formSection.classList.toggle("form_section");
 });
 
-function addBookToLibrary() {}
+closeBtn.addEventListener("click", (e) => {
+  formSection.classList.toggle("form_section");
+});
+
+function displayBook() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    cardTitle.textContent = myLibrary[i].title;
+    cardAuthor.textContent = myLibrary[i].author;
+    cardPages.textContent = myLibrary[i].pages;
+    if (myLibrary[i].isRead) {
+      readToggle.style.color = "black";
+    } else {
+      readToggle.style.color = "red";
+    }
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  let titleValue;
+  let authorValue;
+  let pagesValue;
+  let checkBox;
+  formSection.style.display = "none";
+  e.preventDefault();
+  const formValues = new FormData(e.target);
+  [...formValues.entries()].forEach(([key, val]) => {
+    if (key === "title") {
+      titleValue = val;
+    }
+    if (key === "author") {
+      authorValue = val;
+    }
+    if (key === "pages") {
+      pagesValue = val;
+    }
+    if (key === "checkbox") {
+      if (val === "on") checkBox = true;
+    } else checkBox = false;
+  });
+  let book = new Book(titleValue, authorValue, pagesValue, checkBox);
+  myLibrary.push(book);
+  displayBook();
+});
